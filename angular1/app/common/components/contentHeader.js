a4db1f4ea25baedfaaa0ed2a5c4c88f5962e8caf
Vitor <vit.riber1@hotmail.com>
2020-02-12 17:29:18 -0300

    Finalize the project

--- angular1/app/common/components/contentHeader.js
+++ angular1/app/common/components/contentHeader.js
@@ -1,12 +1,11 @@
 angular.module('primeiraApp').component('contentHeader', {
-    bindings: {
-       name: '@',
-       small: '@',
-    },
-    template: `
-       <section class="content-header">
-         <h1>{{ $ctrl.name }} <small>{{ $ctrl.small }}</small></h1>
-       </section>
-    `
- });
- 
+   bindings: {
+      name: '@',
+      small: '@',
+   },
+   template: `
+      <section class="content-header">
+        <h1>{{ $ctrl.name }} <small>{{ $ctrl.small }}</small></h1>
+      </section>
+   `
+});
--- angular1/app/common/components/field.js
+++ angular1/app/common/components/field.js
@@ -1,27 +1,27 @@
-(function(){
-    angular.module('primeiraApp').component('field', {
-        bindings: {
-            id:'@',
-            label: '@',
-            grid: '@',
-            placeholder: '@',
-            type: '@',
-            model: '=',
-        },
-        controller: [
-            'gridSystem',
-            function(gridSystem){
-                this.gridClasses = gridSystem.toCssClasses(this.grid)
-            }
-        ],
-        template: `
-        <div class="{{$ctrl.gridClasses}}">
-        </div class="form-group">
-            <label for="{{$ctrl.id}}">{{$ctrl.label}}</label>
-            <input id="{{$crtl.id}}" class="form-control" placeholder="{{$ctrl.placeholder}}"
-             type="{{ $ctrl.type }}" ng-model="$ctrl.model"/>
-        </div>
-        </div>
-        `
-    })
-})()
+angular.module('primeiraApp').component('field', {
+  bindings: {
+    id: '@',
+    label: '@',
+    type: '@',
+    grid: '@',
+    model: '=',
+    placeholder: '@',
+    readonly: '<'
+  },
+  controller: [
+    'gridSystem',
+    function(gridSystem) {
+      this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
+    }
+  ],
+  template: `
+   <div class="{{ $ctrl.gridClasses }}">
+     <div class="form-group">
+       <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
+       <input ng-model="$ctrl.model" id="{{ $ctrl.id }}" class="form-control"
+          type="{{ $ctrl.type }}" placeholder="{{ $ctrl.placeholder }}"
+          ng-readonly="$ctrl.readonly" />
+     </div>
+   </div>
+  `
+});
